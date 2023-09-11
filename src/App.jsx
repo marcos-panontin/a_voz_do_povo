07fa4a71eed734190e6516ef730c75658bb4d913
Marcos Panontin <amarcospanontin@gmail.com>
2023-09-11 20:15:47 -0300

    adds footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import getUserAnswersOnPageLoad from './services/getUserAnswersOnPageLoad';
 import QuizResults from './components/QuizResults';
 import checkIfAllQuestionsAreAnswered from './services/checkIfAllQuestionsAreAnswered';
 import findPeopleWithSameAnswers from './services/findPeopleWithSameAnswers';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -32,9 +33,12 @@ function App() {
     <main className='flex flex-col items-center'>
       <PreviousAnswersContext.Provider value={{ previousAnswers, allAnswered, setAllAnswered, peopleWithSameAnswers, setPeopleWithSameAnswers  }}>
         <h1 className='text-5xl text-blue-800'>A Voz Do Povo</h1>
+
+        <h2>Algumas dúvidas assolam o ser humano há milhares de anos. Chegou a hora de encontrar a resposta definitiva para elas.</h2>
         {questions.map((question) => 
           (<Question key={question.question} question={question} />))}
         <QuizResults />
+        <Footer />
       </PreviousAnswersContext.Provider>
     </main>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer() {
+  return (
+    <footer className="fixed bottom-0 text-center bg-blue-700 w-screen text-white font-bold py-1 text-xs">
+      
+      <p>
+      Desenvolvido por Marcos Panontin <span>Github</span> <span>Linkedin</span>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
